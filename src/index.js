f18b6d034e1ea9716933d11ecb6e20b7c08abdf6
ctrlnot <tonton.perocho@gmail.com>
2018-07-30 17:36:32 +0800

    Added more templates and example.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log('loaded index.js');
+/* eslint no-unused-vars: off */
+
+/**
+ *  Import only sass files that is needed to generate a css file.
+ *  Use _ prefixed sass on css utils
+ *
+ *  See: Imports at https://sass-lang.com/guide
+ */
+import style from './styles/main.sass';
+
+/**
+ *  Import all js file here instead at pug files.
+ */
+import plugin from './scripts/plugin';
+
+console.log('lock n loaded!');
