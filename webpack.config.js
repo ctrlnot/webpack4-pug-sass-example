e9f67541462b4c991de164afd99910935f09258a
ctrlnot <tonton.perocho@gmail.com>
2018-07-30 14:11:03 +0800

    Fixed eslint errors.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
-const path = require('path');
-const HtmlWebPackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   module: {
@@ -9,39 +8,39 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
-        }
+          loader: 'babel-loader',
+        },
       },
 
       {
         test: /\.pug$/,
         use: [
-          "html-loader",
-          "pug-html-loader"
-        ]
+          'html-loader',
+          'pug-html-loader',
+        ],
       },
-      
+
       {
         test: /\.sass$/,
         use: [
-            MiniCssExtractPlugin.loader,
-            // "style-loader", // style nodes from js strings
-            "css-loader",
-            "sass-loader"
-        ]
-      }
-    ]
+          MiniCssExtractPlugin.loader,
+          // 'style-loader', // style nodes from js strings
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: "./src/index.pug",
-      filename: "./index.html"
+      template: './src/index.pug',
+      filename: './index.html',
     }),
     new MiniCssExtractPlugin({
       // Options similar to the same options in webpackOptions.output
       // both options are optional
-      filename: "[name].css",
-      chunkFilename: "[id].css"
-    })
-  ]
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+  ],
 };
